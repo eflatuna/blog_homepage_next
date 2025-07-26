7f39fcb06b70b59b904f7e96c2d2b4447505f7a1
esra <esrabaskaya84@gmail.com>
2025-07-26 18:32:23 +0200

    `Added HeroSection component to Home page`

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,9 +1,13 @@
+import HeroSection from "@/components/HeroSection";
 import Navigation from "@/components/Navigation";
 
 export default function Home() {
 	return (
 		<div>
 			<Navigation />
+			<main>
+				<HeroSection />
+			</main>
 		</div>
 	);
 }
